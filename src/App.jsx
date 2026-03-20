c7125a4a9b248fef8ed2e1fc473f722210e4b463
AndresDev20 <andresmarroquin887@gmail.com>
2026-03-20 09:27:42 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import Home from './pages/home/Home'
@@ -17,7 +14,7 @@ function App() {
     <>
      <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/welcome' element={<Welcome/>}/>
+      {/* <Route path='/welcome' element={<Welcome/>}/> */}
       <Route path='/update/:code/:terapeutaId' element={<UpdatePassword/>}/>
       <Route path='/historia/:pacienteId' element={<PacienteId/>}/>
       <Route path='/historia/examenes/:pacienteId' element={<Examenes/>}/>
